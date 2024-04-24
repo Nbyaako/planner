ec7b667ba52a40d97950cd30f91c804840af71d1
Nbyaako <milevskii.2008@mail.ru>
2024-04-24 19:29:23 +0700

    Добавил контроллер

--- application/src/Controller/IndexController.php
+++ application/src/Controller/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends AbstractController
 {
     #[Route('/index', name: 'app_index')]
     public function index(): Response
-    {
+    {   
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
